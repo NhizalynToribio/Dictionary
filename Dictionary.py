bdaa68ea3e4a6a355ad874bca2e85782ffc171fd
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-11-12 21:07:48 +0800

    Done making of the Menu

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,17 @@
 print("**************** PROGRAMMED BY: *****************")
 print("************** NHIZALYN TORIBIO ****************")
 print("***************** BSCOE 2 -2 *******************")
-print("********** ASSIGNMENT IN DS ****************")
+print("********** ASSIGNMENT (DICTIONARY) ****************")
+
+# Shows the Menu of the Program
+print("""\n
+        =============== HELLO WELCOME ===============
+        -------------- CONTACT TRACING --------------
+        \n          MENU
+        1 -> ADD AN ITEM         [1]
+        2 -> SEARCH              [2]
+        3 -> EXIT                [3]
+        =============================================
+        ---------------------------------------------
+        """)
+

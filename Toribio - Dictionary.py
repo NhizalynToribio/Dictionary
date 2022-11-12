05cb85733f31ad6e82280955b5e9e746a3843755
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-11-12 23:32:23 +0800

    Dictionary

--- Toribio - Dictionary.py
+++ Toribio - Dictionary.py
@@ -2,3 +2,6 @@ print("**************** PROGRAMMED BY: *****************")
 print("************** NHIZALYN TORIBIO ****************")
 print("***************** BSCOE 2 -2 *******************")
 print("********** ASSIGNMENT (DICTIONARY) ****************")
+
+# This is the Dictionary to store the Contacts
+ContactDictionary = {}

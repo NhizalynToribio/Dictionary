6b1e697f7009a1590371a7505d20fb5dada5063c
Nhizalyn Toribio <116430536+NhizalynToribio@users.noreply.github.com>
2022-11-19 17:43:20 +0800

    Delete Interesting.py

--- Interesting.py
+++
@@ -1,5 +0,0 @@
-print("**************** PROGRAMMED BY: *****************")
-print("************** NHIZALYN TORIBIO ****************")
-print("***************** BSCOE 2 -2 *******************")
-print("******** SEATWORK (EXCITING WEBSITE) ***********")
-
